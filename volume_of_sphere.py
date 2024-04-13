2ee296e7d189d7449ff44716daee692818c917a7
Whatro <166838774+Whatro@users.noreply.github.com>
2024-04-13 18:48:19 -0500

    Create volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,12 @@
+import math 
+
+def calculate_volume_of_sphere(radius):
+  return (4/3) * math.pi * radius ** 3
+radii_one = 30
+radii_two = 40
+
+first_calculation = calculate_volume_of_sphere(radii_one)
+second_calculation = calculate_volume_of_sphere(radii_two)
+
+print(first_calculation)
+print(second_calculation)
